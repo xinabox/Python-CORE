def7b83d80450d4304f9ad8e5f91a222c095fe9c
luqmaanb <lb@xinabox.cc>
2020-06-21 14:04:12 +0200

    Add smbus2 dependency for pi

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import setuptools
+import sys
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+install_requires = list()
+if sys.platform == "linux2" or sys.platform == "linux":
+    install_requires = ["smbus2",]
+
 setuptools.setup(
     name="xinabox-CORE",
-    version="0.0.6",
+    version="0.0.7",
     author="Luqmaan Baboo",
     author_email="luqmaanbaboo@gmail.com",
     description="I2C Core for CC03/CS11/CW03, CW02, CW01, Raspberry Pi and Microbit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xinabox/Python-CORE",
+    install_requires=install_requires,
     py_modules=["xCore",],
     packages=setuptools.find_packages(),
     classifiers=[
