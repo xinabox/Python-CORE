5cd263ee48698d4e626110b8d6d3933f82800321
luqmaanb <lb@xinabox.cc>
2020-06-21 12:56:12 +0200

    py_modules

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xinabox-CORE",
-    version="0.0.5",
+    version="0.0.6",
     author="Luqmaan Baboo",
     author_email="luqmaanbaboo@gmail.com",
     description="I2C Core for CC03/CS11/CW03, CW02, CW01, Raspberry Pi and Microbit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xinabox/Python-CORE",
+    py_modules=["xCore",],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
